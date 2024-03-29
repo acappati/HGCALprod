72d5f53adb6f2bf35232dc8b7d31226bc60d2ce8
Alessandra Cappati <alessandra.cappati@cern.ch>
2024-03-29 18:51:15 +0100

    add step1 and step2 cfg for ticlv5

--- SingleElectronFlatPt2To100_cfi_GEN_SIM.py
+++ SingleElectronFlatPt2To100_cfi_GEN_SIM.py
@@ -1,35 +1,13 @@
-# Auto generated configuration file [varParsing added February 2023]
-# using:
-# Revision: 1.19
-# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v
-# with command line options: CloseByParticle_Photon_ERZRanges_cfi --conditions auto:phase2_realistic_T15 -n 10 --era Phase2C9 --eventcontent FEVTDEBUG --relval 9000,100 -s GEN,SIM --datatier GEN-SIM --beamspot HLLHC --geometry Extended2026D49 --fileout file:step1.root
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: SingleElectronFlatPt2To100_cfi -s GEN,SIM -n 10 --conditions auto:phase2_realistic_T25 --beamspot HLLHC --datatier GEN-SIM --eventcontent FEVTDEBUG --geometry Extended2026D98 --era Phase2C17I13M9 --no_exec --fileout file:step1.root --nThreads 8
 import FWCore.ParameterSet.Config as cms
 
-from FWCore.ParameterSet.VarParsing import VarParsing
-FLAGS = VarParsing('analysis')
-# add a list of strings for events to process
-FLAGS.register('nEvents',
-               100,
-               VarParsing.multiplicity.singleton,
-               VarParsing.varType.int,
-               "Number of events to process.")
-FLAGS.register('seed',
-               1,
-               VarParsing.multiplicity.singleton,
-               VarParsing.varType.int,
-               "Initial generator seed.")
-FLAGS.register('PID',
-               22,
-               VarParsing.multiplicity.singleton,
-               VarParsing.varType.int,
-               "PID of the particle to generate.")
-FLAGS.parseArguments()
-
-#from Configuration.Eras.Era_Phase2C9_cff import Phase2C9
 from Configuration.Eras.Era_Phase2C17I13M9_cff import Phase2C17I13M9
-#process = cms.Process('SIM',Phase2C9)
-process = cms.Process('SIM',Phase2C17I13M9)
 
+process = cms.Process('SIM',Phase2C17I13M9)
 
 # import of standard configurations
 process.load('Configuration.StandardSequences.Services_cff')
@@ -37,10 +15,8 @@ process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
 process.load('FWCore.MessageService.MessageLogger_cfi')
 process.load('Configuration.EventContent.EventContent_cff')
 process.load('SimGeneral.MixingModule.mixNoPU_cfi')
-#process.load('Configuration.Geometry.GeometryExtended2026D49Reco_cff')
-#process.load('Configuration.Geometry.GeometryExtended2026D49_cff')
-process.load('Configuration.Geometry.GeometryExtended2026D88_cff')
-process.load('Configuration.Geometry.GeometryExtended2026D88Reco_cff')
+process.load('Configuration.Geometry.GeometryExtended2026D98Reco_cff')
+process.load('Configuration.Geometry.GeometryExtended2026D98_cff')
 process.load('Configuration.StandardSequences.MagneticField_cff')
 process.load('Configuration.StandardSequences.Generator_cff')
 #process.load('IOMC.EventVertexGenerators.VtxSmearedHLLHC_cfi')
@@ -51,35 +27,39 @@ process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
 # random seeds
-process.RandomNumberGeneratorService.generator.initialSeed = cms.untracked.uint32(FLAGS.seed)
-
+#process.RandomNumberGeneratorService.generator.initialSeed = cms.untracked.uint32(FLAGS.seed)
 
 process.maxEvents = cms.untracked.PSet(
-        input = cms.untracked.int32(FLAGS.nEvents),
-        output = cms.optional.untracked.allowed(cms.int32,cms.PSet)
+    input = cms.untracked.int32(10),
+    output = cms.optional.untracked.allowed(cms.int32,cms.PSet)
 )
 
 # Input source
 process.source = cms.Source("EmptySource")
 
 process.options = cms.untracked.PSet(
-    FailPath = cms.untracked.vstring(),
     IgnoreCompletely = cms.untracked.vstring(),
     Rethrow = cms.untracked.vstring(),
-    SkipEvent = cms.untracked.vstring(),
+    TryToContinue = cms.untracked.vstring(),
+    accelerators = cms.untracked.vstring('*'),
     allowUnscheduled = cms.obsolete.untracked.bool,
     canDeleteEarly = cms.untracked.vstring(),
+    deleteNonConsumedUnscheduledModules = cms.untracked.bool(True),
+    dumpOptions = cms.untracked.bool(False),
     emptyRunLumiMode = cms.obsolete.untracked.string,
     eventSetup = cms.untracked.PSet(
         forceNumberOfConcurrentIOVs = cms.untracked.PSet(
             allowAnyLabel_=cms.required.untracked.uint32
         ),
-        numberOfConcurrentIOVs = cms.untracked.uint32(1)
+        numberOfConcurrentIOVs = cms.untracked.uint32(0)
     ),
     fileMode = cms.untracked.string('FULLMERGE'),
     forceEventSetupCacheClearOnNewRun = cms.untracked.bool(False),
+    holdsReferencesToDeleteEarly = cms.untracked.VPSet(),
     makeTriggerResults = cms.obsolete.untracked.bool,
-    numberOfConcurrentLuminosityBlocks = cms.untracked.uint32(1),
+    modulesToCallForTryToContinue = cms.untracked.vstring(),
+    modulesToIgnoreForDeleteEarly = cms.untracked.vstring(),
+    numberOfConcurrentLuminosityBlocks = cms.untracked.uint32(0),
     numberOfConcurrentRuns = cms.untracked.uint32(1),
     numberOfStreams = cms.untracked.uint32(0),
     numberOfThreads = cms.untracked.uint32(1),
@@ -116,7 +96,7 @@ process.FEVTDEBUGoutput = cms.OutputModule("PoolOutputModule",
 # Other statements
 process.genstepfilter.triggerConditions=cms.vstring("generation_step")
 from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic_T21', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic_T25', '')
 
 process.generator = cms.EDProducer("CloseByParticleGunProducer",
     AddAntiParticle = cms.bool(True),
@@ -132,7 +112,7 @@ process.generator = cms.EDProducer("CloseByParticleGunProducer",
         MinPhi = cms.double(-3.14159265359),
         NParticles = cms.int32(1),
         Overlapping = cms.bool(False),
-        PartID = cms.vint32(FLAGS.PID),
+        PartID = cms.vint32(22),
         Pointing = cms.bool(True),
         # RMax = cms.double(127),
         # RMin = cms.double(41),
@@ -157,6 +137,12 @@ process.FEVTDEBUGoutput_step = cms.EndPath(process.FEVTDEBUGoutput)
 process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.simulation_step,process.endjob_step,process.FEVTDEBUGoutput_step)
 from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
 associatePatAlgosToolsTask(process)
+
+#Setup FWK for multithreaded
+process.options.numberOfThreads = 8
+process.options.numberOfStreams = 0
+process.options.numberOfConcurrentLuminosityBlocks = 1
+process.options.eventSetup.numberOfConcurrentIOVs = 1
 # filter all path with the production filter sequence
 for path in process.paths:
 	getattr(process,path).insert(0, process.generator)
--- step2_DIGI_L1TrackTrigger_L1_DIGI2RAW_HLT_PU.py
+++ step2_DIGI_L1TrackTrigger_L1_DIGI2RAW_HLT_PU.py
@@ -1,12 +1,12 @@
 # Auto generated configuration file
-# using:
-# Revision: 1.19
-# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v
-# with command line options: step2 --conditions auto:phase2_realistic_T15 --pileup_input das:/RelValMinBias_14TeV/CMSSW_11_2_0_pre8-112X_mcRun4_realistic_v3_2026D49noPU-v1/GEN-SIM -n 10 --era Phase2C9 --eventcontent FEVTDEBUGHLT -s DIGI:pdigi_valid,L1TrackTrigger,L1,DIGI2RAW,HLT:@fake2 --datatier GEN-SIM-DIGI-RAW --pileup AVE_200_BX_25ns --geometry Extended2026D49 --no_exec --filein file:step1.root --fileout file:step2.root
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: step2 -s DIGI:pdigi_valid,L1TrackTrigger,L1,DIGI2RAW,HLT:@fake2 --conditions auto:phase2_realistic_T25 --datatier GEN-SIM-DIGI-RAW -n 1000 --eventcontent FEVTDEBUGHLT --geometry Extended2026D98 --era Phase2C17I13M9 --pileup AVE_200_BX_25ns --pileup_input das:/RelValMinBias_14TeV/CMSSW_13_2_0_pre1-131X_mcRun4_realistic_v5_2026D98noPU-v1/GEN-SIM --no_exec --filein file:step1.root --fileout file:step2.root --nThreads 8
 import FWCore.ParameterSet.Config as cms
 
-#from Configuration.Eras.Era_Phase2C9_cff import Phase2C9
 from Configuration.Eras.Era_Phase2C17I13M9_cff import Phase2C17I13M9
+
 process = cms.Process('HLT',Phase2C17I13M9)
 
 # import of standard configurations
@@ -15,9 +15,7 @@ process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
 process.load('FWCore.MessageService.MessageLogger_cfi')
 process.load('Configuration.EventContent.EventContent_cff')
 process.load('SimGeneral.MixingModule.mix_POISSON_average_cfi')
-#process.load('Configuration.Geometry.GeometryExtended2026D49Reco_cff')
-process.load('Configuration.Geometry.GeometryExtended2026D88_cff')
-process.load('Configuration.Geometry.GeometryExtended2026D88Reco_cff')
+process.load('Configuration.Geometry.GeometryExtended2026D98Reco_cff')
 process.load('Configuration.StandardSequences.MagneticField_cff')
 process.load('Configuration.StandardSequences.Digi_cff')
 process.load('Configuration.StandardSequences.L1TrackTrigger_cff')
@@ -28,7 +26,7 @@ process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(-1),
+    input = cms.untracked.int32(1000),
     output = cms.optional.untracked.allowed(cms.int32,cms.PSet)
 )
 
@@ -59,23 +57,28 @@ process.source = cms.Source("PoolSource",
 )
 
 process.options = cms.untracked.PSet(
-    FailPath = cms.untracked.vstring(),
     IgnoreCompletely = cms.untracked.vstring(),
     Rethrow = cms.untracked.vstring(),
-    SkipEvent = cms.untracked.vstring(),
+    TryToContinue = cms.untracked.vstring(),
+    accelerators = cms.untracked.vstring('*'),
     allowUnscheduled = cms.obsolete.untracked.bool,
     canDeleteEarly = cms.untracked.vstring(),
+    deleteNonConsumedUnscheduledModules = cms.untracked.bool(True),
+    dumpOptions = cms.untracked.bool(False),
     emptyRunLumiMode = cms.obsolete.untracked.string,
     eventSetup = cms.untracked.PSet(
         forceNumberOfConcurrentIOVs = cms.untracked.PSet(
             allowAnyLabel_=cms.required.untracked.uint32
         ),
-        numberOfConcurrentIOVs = cms.untracked.uint32(1)
+        numberOfConcurrentIOVs = cms.untracked.uint32(0)
     ),
     fileMode = cms.untracked.string('FULLMERGE'),
     forceEventSetupCacheClearOnNewRun = cms.untracked.bool(False),
+    holdsReferencesToDeleteEarly = cms.untracked.VPSet(),
     makeTriggerResults = cms.obsolete.untracked.bool,
-    numberOfConcurrentLuminosityBlocks = cms.untracked.uint32(1),
+    modulesToCallForTryToContinue = cms.untracked.vstring(),
+    modulesToIgnoreForDeleteEarly = cms.untracked.vstring(),
+    numberOfConcurrentLuminosityBlocks = cms.untracked.uint32(0),
     numberOfConcurrentRuns = cms.untracked.uint32(1),
     numberOfStreams = cms.untracked.uint32(0),
     numberOfThreads = cms.untracked.uint32(1),
@@ -87,7 +90,7 @@ process.options = cms.untracked.PSet(
 
 # Production Info
 process.configurationMetadata = cms.untracked.PSet(
-    annotation = cms.untracked.string('step2 nevts:10'),
+    annotation = cms.untracked.string('step2 nevts:1000'),
     name = cms.untracked.string('Applications'),
     version = cms.untracked.string('$Revision: 1.19 $')
 )
@@ -111,16 +114,10 @@ process.mix.input.nbPileupEvents.averageNumber = cms.double(200.000000)
 process.mix.bunchspace = cms.int32(25)
 process.mix.minBunch = cms.int32(-3)
 process.mix.maxBunch = cms.int32(3)
-
-
-
-process.mix.input.fileNames = cms.untracked.vstring(['/store/relval/CMSSW_12_3_0_pre5/RelValMinBias_14TeV/GEN-SIM/123X_mcRun4_realistic_v4_2026D88noPU-v1/10000/066fc95d-1cef-4469-9e08-3913973cd4ce.root', '/store/relval/CMSSW_12_3_0_pre5/RelValMinBias_14TeV/GEN-SIM/123X_mcRun4_realistic_v4_2026D88noPU-v1/10000/07928a25-231b-450d-9d17-e20e751323a1.root', '/store/relval/CMSSW_12_3_0_pre5/RelValMinBias_14TeV/GEN-SIM/123X_mcRun4_realistic_v4_2026D88noPU-v1/10000/26bd8fb0-575e-4201-b657-94cdcb633045.root', '/store/relval/CMSSW_12_3_0_pre5/RelValMinBias_14TeV/GEN-SIM/123X_mcRun4_realistic_v4_2026D88noPU-v1/10000/4206a9c5-44c2-45a5-aab2-1a8a6043a08a.root', '/store/relval/CMSSW_12_3_0_pre5/RelValMinBias_14TeV/GEN-SIM/123X_mcRun4_realistic_v4_2026D88noPU-v1/10000/55a372bf-a234-4111-8ce0-ead6157a1810.root', '/store/relval/CMSSW_12_3_0_pre5/RelValMinBias_14TeV/GEN-SIM/123X_mcRun4_realistic_v4_2026D88noPU-v1/10000/59ad346c-f405-4288-96d7-795f81c43fe8.root', '/store/relval/CMSSW_12_3_0_pre5/RelValMinBias_14TeV/GEN-SIM/123X_mcRun4_realistic_v4_2026D88noPU-v1/10000/7280f5ec-b71d-4579-a730-7ce2de0ff906.root', '/store/relval/CMSSW_12_3_0_pre5/RelValMinBias_14TeV/GEN-SIM/123X_mcRun4_realistic_v4_2026D88noPU-v1/10000/b93adc85-715f-477a-afc9-65f3241933ee.root', '/store/relval/CMSSW_12_3_0_pre5/RelValMinBias_14TeV/GEN-SIM/123X_mcRun4_realistic_v4_2026D88noPU-v1/10000/c7a0aa46-f55c-4b01-977f-34a397b71fba.root', '/store/relval/CMSSW_12_3_0_pre5/RelValMinBias_14TeV/GEN-SIM/123X_mcRun4_realistic_v4_2026D88noPU-v1/10000/e77fa467-97cb-4943-884f-6965b4eb0390.root'])
-
-
-
+process.mix.input.fileNames = cms.untracked.vstring(['/store/relval/CMSSW_13_2_0_pre1/RelValMinBias_14TeV/GEN-SIM/131X_mcRun4_realistic_v5_2026D98noPU-v1/2590000/019a855c-d70e-42f6-b953-550e83ded2c9.root', '/store/relval/CMSSW_13_2_0_pre1/RelValMinBias_14TeV/GEN-SIM/131X_mcRun4_realistic_v5_2026D98noPU-v1/2590000/1e6ae6fd-8ac7-4eca-b727-3252ae4ff52f.root', '/store/relval/CMSSW_13_2_0_pre1/RelValMinBias_14TeV/GEN-SIM/131X_mcRun4_realistic_v5_2026D98noPU-v1/2590000/313dfade-529e-4ae1-9f42-4813b8c83862.root', '/store/relval/CMSSW_13_2_0_pre1/RelValMinBias_14TeV/GEN-SIM/131X_mcRun4_realistic_v5_2026D98noPU-v1/2590000/7d4ca041-1a5d-4580-b5fd-fedeb7c61e88.root', '/store/relval/CMSSW_13_2_0_pre1/RelValMinBias_14TeV/GEN-SIM/131X_mcRun4_realistic_v5_2026D98noPU-v1/2590000/c04e329b-d890-4f57-8aeb-f9a671ae0312.root', '/store/relval/CMSSW_13_2_0_pre1/RelValMinBias_14TeV/GEN-SIM/131X_mcRun4_realistic_v5_2026D98noPU-v1/2590000/d1e561ab-33b8-48d6-b721-f1651d821732.root', '/store/relval/CMSSW_13_2_0_pre1/RelValMinBias_14TeV/GEN-SIM/131X_mcRun4_realistic_v5_2026D98noPU-v1/2590000/d83dc109-bd09-4049-a9ac-49c2f2d922b7.root', '/store/relval/CMSSW_13_2_0_pre1/RelValMinBias_14TeV/GEN-SIM/131X_mcRun4_realistic_v5_2026D98noPU-v1/2590000/f20bedd8-3db8-4d04-a2ab-0360e175f3ad.root', '/store/relval/CMSSW_13_2_0_pre1/RelValMinBias_14TeV/GEN-SIM/131X_mcRun4_realistic_v5_2026D98noPU-v1/2590000/f2ac7802-1364-45b1-b21d-f5f32f3bd20d.root'])
 process.mix.digitizers = cms.PSet(process.theDigitizersValid)
 from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic_T21', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic_T25', '')
 
 # Path and EndPath definitions
 process.digitisation_step = cms.Path(process.pdigi_valid)
@@ -131,30 +128,23 @@ process.endjob_step = cms.EndPath(process.endOfProcess)
 process.FEVTDEBUGHLToutput_step = cms.EndPath(process.FEVTDEBUGHLToutput)
 
 # Schedule definition
-
+# process.schedule imported from cff in HLTrigger.Configuration
 process.schedule.insert(0, process.digitisation_step)
 process.schedule.insert(1, process.L1TrackTrigger_step)
 process.schedule.insert(2, process.L1simulation_step)
 process.schedule.insert(3, process.digi2raw_step)
 process.schedule.extend([process.endjob_step,process.FEVTDEBUGHLToutput_step])
-
-
-
-
-'''
-process.schedule = cms.Schedule(process.digitisation_step,process.L1TrackTrigger_step,process.L1simulation_step,process.digi2raw_step)
-process.schedule.extend(process.HLTSchedule)
-process.schedule.extend([process.endjob_step,process.FEVTDEBUGHLToutput_step])
-'''
-
-
 from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
 associatePatAlgosToolsTask(process)
+
+#Setup FWK for multithreaded
+process.options.numberOfThreads = 8
+process.options.numberOfStreams = 0
 
 # customisation of the process.
 
 # Automatic addition of the customisation function from HLTrigger.Configuration.customizeHLTforMC
-from HLTrigger.Configuration.customizeHLTforMC import customizeHLTforMC
+from HLTrigger.Configuration.customizeHLTforMC import customizeHLTforMC 
 
 #call to customisation function customizeHLTforMC imported from HLTrigger.Configuration.customizeHLTforMC
 process = customizeHLTforMC(process)
